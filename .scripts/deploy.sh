d85b68d6c076ec335054ce83f89defc216e09c2e
Qamar Ali <qamarali205@gmail.com>
2024-01-25 10:05:41 +0530

    test

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,13 +4,13 @@ set -e
 echo "Deployment started..."
 
 # Pull the latest version of the app
-git pull origin main
+git pull origin master
 echo "New changes copied to server !"
 
 echo "Installing Dependencies..."
 npm install --yes
 
-echo "Creating Production Build..."
-npm start
+echo "PM2 Reload"
+pm2 reload api.js
 
 echo "Deployment Finished!"
