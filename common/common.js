8421fe0c98e5de24e849ff45a5980f301780a7a2
Qamar Ali <qamarali205@gmail.com>
2024-01-25 11:21:28 +0530

    testing

--- common/common.js
+++ common/common.js
@@ -6,7 +6,7 @@ exports.mongoUrl = {
 };
 
 exports.actionCompleteResponse = function (res, data, msg) {
-  // console.log(data);
+  // console.log(dat);
   var response = {
     success: 1,
     message: msg || responseMessages.ACTION_COMPLETE,
