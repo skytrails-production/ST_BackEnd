fd9fb707d82d9870e30702e4c411a1863c9bc26d
Qamar Ali <qamarali205@gmail.com>
2024-01-25 11:31:01 +0530

    jjhj

--- api.js
+++ api.js
@@ -97,7 +97,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Hello Production DB Updated",
+    message: "Hellooo Production DB Updated",
     url: `${req.protocol}://${req.get("host")}`,
   });
 });
