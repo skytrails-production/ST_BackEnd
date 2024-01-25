46bab543df6d408e3d93aaf770a9befe57fbf500
Qamar Ali <qamarali205@gmail.com>
2024-01-25 11:06:18 +0530

    latest

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,13 +4,13 @@ set -e
 echo "Deployment started..."
 
 # Pull the latest version of the app
-git pull origin master
+git pull origin main
 echo "New changes copied to server !"
 
 echo "Installing Dependencies..."
 npm install --yes
 
 echo "PM2 Reload"
-pm2 reload api.js
+pm2 reload Skytrails-backend
 
 echo "Deployment Finished!"
