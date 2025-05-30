a5c780e1e4fec761fbea76d8f34220fd30ca8973
Qamar Ali <qamarali205@gmail.com>
2025-05-30 16:23:18 +0530

    check ci/cd

--- api.js
+++ api.js
@@ -129,7 +129,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Hello Production DB Updated",
+    message: "Hello000 Production DB Updated",
     url: `${req.protocol}://${req.get("host")}`,
   });
 });
