1e5297507cfe2ccfa6b50c8f7182346c025a86df
Qamar Ali <qamarali205@gmail.com>
2025-05-31 10:25:19 +0530

    check ci/cd with vs code

--- api.js
+++ api.js
@@ -129,7 +129,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Hello000 Production DB Updated",
+    message: "Hello Production DB Updated",
     url: `${req.protocol}://${req.get("host")}`,
   });
 });
